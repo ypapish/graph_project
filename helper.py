2a1e31da63b7ece1e92c79ecd2c87a8a808df2f8
Yana <155889900+ypapish@users.noreply.github.com>
2024-12-31 02:44:34 +0200

    add create_window function

--- helper.py
+++ helper.py
@@ -1,1 +1,10 @@
+import tkinter as tk
 
+
+def create_window(title, width, height):
+    window = tk.Tk()
+    window.geometry(f'{width}x{height}')
+    window.title(title)
+    canvas = tk.Canvas(window, borderwidth=0, highlightthickness=0)
+    canvas.pack(fill=tk.BOTH, expand=1)
+    return window, canvas
