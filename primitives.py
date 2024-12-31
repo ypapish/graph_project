c2458562663048ad5c868174ca5f315289f5cf99
Yana <155889900+ypapish@users.noreply.github.com>
2024-12-31 02:53:45 +0200

    add get_vertex_closure function

--- primitives.py
+++ primitives.py
@@ -25,3 +25,16 @@ def get_coord(x, y, dist, angle):
     x1 = x + dist * cos(angle)
     y1 = y + dist * sin(angle)
     return x1, y1
+
+
+def get_vertex_closure(count, size, radius):
+    x_center, y_center = size / 2, size / 2
+    length = size / 2 - 1.3 * radius
+    step = 2 * pi / (count - 1)
+    def get_vertex(index):
+      if index == 0:
+        return Vertex(size / 2, size / 2, 0)
+      angle = (index - 1) * step
+      x, y = get_coord(x_center, y_center, length, angle)
+      return Vertex(x, y, index)
+    return get_vertex
