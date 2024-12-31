03559ff4092273ed90ac878ee5deb1645c0e5701
Yana <155889900+ypapish@users.noreply.github.com>
2024-12-31 02:57:39 +0200

    add is_opposite function

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from config import WIDTH, HEIGHT, VER_NUM, seed, k, RADIUS
 from primitives import get_vertex_closure, draw_vertex, line_connect, loop, arc_connect
 import math
 
+
+def is_opposite(count, i, j):
+  half = math.ceil((count - 1) / 2)
+  if i > j: (i, j) = (j, i)
+  return i == j - half
+
