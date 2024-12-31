a3260ba61cc09cb5caf132f95f8d5db702279e3e
Yana <155889900+ypapish@users.noreply.github.com>
2024-12-31 02:51:50 +0200

    add draw_vertex function

--- primitives.py
+++ primitives.py
@@ -13,3 +13,9 @@ def draw_circle(canvas, x, y):
     x1, y1 = x - RADIUS, y - RADIUS
     x2, y2 = x + RADIUS, y + RADIUS
     canvas.create_oval(x1, y1, x2, y2, width=2,fill="pink",outline="pink")
+
+
+def draw_vertex(canvas, vertex):
+    x, y = vertex.x, vertex.y
+    draw_circle(canvas, x, y)
+    canvas.create_text(x, y, text=vertex.name, font=5)
