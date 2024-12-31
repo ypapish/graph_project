509101fa07e3174ebc2e7923ac21b62fcc63f749
Yana <155889900+ypapish@users.noreply.github.com>
2024-12-31 02:56:48 +0200

    add imports

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+from helper import create_window, generate_matrix, to_undirected, print_matrix
+from config import WIDTH, HEIGHT, VER_NUM, seed, k, RADIUS
+from primitives import get_vertex_closure, draw_vertex, line_connect, loop, arc_connect
+import math
 
