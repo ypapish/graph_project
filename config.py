5a0706b016808316d2b1712f5de1ba6c58754084
Yana <155889900+ypapish@users.noreply.github.com>
2024-12-31 02:55:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+n1, n2, n3, n4 = 3, 1, 1, 7
 
+RADIUS = 40
+WIDTH = 800
+HEIGHT = 800
+VER_NUM = 10 + n3
+SIDES = 4
+
+k = 1 - n3 * 0.02 - n4 * 0.005 - 0.25
+
+seed = 3117
+
+arrowshape = (8, 10, 4)
