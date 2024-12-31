64e4537fa47af9e25dc3ea2fc04fd5c2500dda4a
Yana <155889900+ypapish@users.noreply.github.com>
2024-12-31 02:45:08 +0200

    add print_matrix

--- helper.py
+++ helper.py
@@ -8,3 +8,10 @@ def create_window(title, width, height):
     canvas = tk.Canvas(window, borderwidth=0, highlightthickness=0)
     canvas.pack(fill=tk.BOTH, expand=1)
     return window, canvas
+
+
+def print_matrix(matrix):
+    for row in matrix:
+      for n in row:
+        print(n, end = ' ')
+      print()
