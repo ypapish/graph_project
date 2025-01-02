8b18421f7f4ce9fa85b39e687361d6dd5127104e
Yana <y.papish14@gmail.com>
2025-01-02 15:24:02 +0200

    update draw_graph function

--- main.py
+++ main.py
@@ -11,41 +11,23 @@ def is_opposite(count, i, j):
     return i == j - half
 
 
-def draw_graph_directed(canvas, matrix, radius, screen_width):
+def draw_graph(canvas, matrix, radius, screen_width, directed=False):
     count = len(matrix)
     get_vertex = get_vertex_closure(count, screen_width, radius)
     for i in range(count):
         vertex = get_vertex(i)
         draw_vertex(canvas, vertex)
-        for j in range(count):
+        for j in (range(count) if directed else range(i + 1)):
             if matrix[i][j] == 0:
                 continue
             other_vertex = get_vertex(j)
             if i == j:
-                loop(canvas, vertex, True)
+                loop(canvas, vertex, directed)
             elif is_opposite(count, i, j):
-                arc_connect(canvas, vertex, other_vertex, True)
+                arc_connect(canvas, vertex, other_vertex, directed)
             else:
-                shift = i > j and matrix[j][i]
-                line_connect(canvas, vertex, other_vertex, True, shift)
-
-
-def draw_graph_undirected(canvas, matrix, radius, screen_width):
-    count = len(matrix)
-    get_vertex = get_vertex_closure(count, screen_width, radius)
-    for i in range(count):
-        vertex = get_vertex(i)
-        draw_vertex(canvas, vertex)
-        for j in range(i + 1):
-            if matrix[i][j] == 0:
-                continue
-            other_vertex = get_vertex(j)
-            if i == j:
-                loop(canvas, vertex)
-            elif is_opposite(count, i, j):
-                arc_connect(canvas, vertex, other_vertex)
-            else:
-                line_connect(canvas, vertex, other_vertex)
+                shift = directed and i > j and matrix[j][i]
+                line_connect(canvas, vertex, other_vertex, directed, shift)
 
 
 def main(canvas,type):
